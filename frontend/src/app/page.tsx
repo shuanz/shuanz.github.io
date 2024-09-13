3b57f8e61fc0d2f33e1d76801fc510374837dfbc
shuanz <shuanz@gmail.com>
2024-09-13 00:24:11 +0000

    Black Friday

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,10 @@
-export default function Home() {
+import { blackFridayFlag } from './flag';
+
+export default async function Page() {
+  const black_friday = await blackFridayFlag();
+
   return (
+<<<<<<< HEAD
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <h1> Welcome to the ConchaYOro App deploy v2.0 </h1>
@@ -7,3 +12,15 @@ export default function Home() {
     </main>
   )
 }
+=======
+    <html>
+      <body>
+        <div>
+          <h1> Welcome to the ConchaYOro App - deploy v3a</h1>
+          { black_friday ? <button> Promo </button> : <button> Normal </button> } 
+        </div>
+      </body>
+    </html>
+  )
+};
+>>>>>>> 9a5e105 (Black Friday)
